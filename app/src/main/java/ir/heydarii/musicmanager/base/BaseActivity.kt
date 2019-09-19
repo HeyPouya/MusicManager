1d737d833bf7c2f13b9c32b55c4e27022aec7635
Pouya <pouyaheydary@gmail.com>
2019-09-19 19:57:50 +0430

    changed a comment in baseActivity

--- app/src/main/java/ir/heydarii/musicmanager/base/BaseActivity.kt
+++ app/src/main/java/ir/heydarii/musicmanager/base/BaseActivity.kt
@@ -2,8 +2,9 @@ package ir.heydarii.musicmanager.base
 
 import androidx.appcompat.app.AppCompatActivity
 
-/*
-All activities inherit this class, so our hands will be open in the future
-to do some common things in them
+/**
+ *
+ * All activities inherit this class, so our hands will be open in the future
+ * to do some common things in them
  */
 open class BaseActivity : AppCompatActivity()
